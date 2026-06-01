7f20209fbda3e1695c89f06f833136a7458aa851
zhuhuagang1981-sudo <zhuhuagang1981@gmail.com>
2026-06-01 19:59:20 +0800

    feat: v2 - __init__.py - 五书知识库升级

--- gongyafu-agent/agent/__init__.py
+++ gongyafu-agent/agent/__init__.py
@@ -4,18 +4,48 @@
 
 from .system_prompt import SYSTEM_PROMPT, OFF_TOPIC_PROMPT, PROBE_DECISION_PROMPT
 from .knowledge_base import KNOWLEDGE_BASE
-from .boundary_guard import is_teaching_related, get_redirection_message
-from .dialogue_engine import DialogueEngine, DialogueStage
+from .boundary_guard import (
+    is_teaching_related,
+    get_redirection_message,
+    detect_grade_level,
+    get_topic_classification,
+)
+from .dialogue_engine import (
+    DialogueEngine,
+    DialogueStage,
+    SourceTracking,
+    CaseGuidance,
+    GradeLevelAwareness,
+    book_cross_reference,
+    BOOK_TASK,
+    BOOK_FOUNDATION_1,
+    BOOK_FOUNDATION_2,
+    BOOK_PRIMARY,
+    BOOK_JUNIOR,
+    BOOK_NAMES,
+)
 from .response_formatter import format_response
 
 __all__ = [
     "SYSTEM_PROMPT",
-    "OFF_TOPIC_PROMPT", 
+    "OFF_TOPIC_PROMPT",
     "PROBE_DECISION_PROMPT",
     "KNOWLEDGE_BASE",
     "is_teaching_related",
     "get_redirection_message",
+    "detect_grade_level",
+    "get_topic_classification",
     "DialogueEngine",
     "DialogueStage",
+    "SourceTracking",
+    "CaseGuidance",
+    "GradeLevelAwareness",
+    "book_cross_reference",
+    "BOOK_TASK",
+    "BOOK_FOUNDATION_1",
+    "BOOK_FOUNDATION_2",
+    "BOOK_PRIMARY",
+    "BOOK_JUNIOR",
+    "BOOK_NAMES",
     "format_response",
 ]
